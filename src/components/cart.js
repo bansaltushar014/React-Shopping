6c9918a8cfbe966d8c7849697b96d73f20eecf0e
bansaltushar014 <bansaltushar014@gmail.com>
2020-10-10 03:41:44 +0530

    Cart & Animation

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,13 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import formatCurrency from '../helper/currency';
+import Fade from 'react-reveal/Fade';
 
-function Cart() {
+function Cart(props) {
 
-    
+    const [proceed, setProceed] = useState(false);
+
+    // useEffect(() =>{
+       
+    // },[]) 
+
+    console.log(props.cardItems);
+
+    const funProceed = () => {
+        setProceed(true);
+    }
+
     return (
         <>
-        Cart
+        <ul class="collection">
+            {
+                props.cardItems.map((item, index) => {
+                    return <Fade left cascade>
+                    <li class="collection-item avatar">
+                    <img style={{ maxWidth: 30 }} src={item.image} alt="" class="circle"/>
+                    <span class="title">Title {item.title}</span>
+                    <p>Price {formatCurrency(item.price)} * {item.count} = {formatCurrency(item.price * (item.count))}<br/>
+                    <a class="btn-floating btn-small waves-effect waves-light red" onClick={() => {props.removeFromCart(item._id)}}><i class="material-icons">clear</i></a>
+                    </p>
+                  </li>
+                  </Fade>
+                 })
+             }
+        </ul>     
+             
+             Total : {formatCurrency(props.cardItems.reduce((a, c) => a + c.price * c.count, 0))} <br/>
+            
+
+            { props.cardItems.length && 
+                <button onClick={funProceed}> Proceed </button>             
+            }
+
+            {proceed &&
+                <div class="row">
+                <Fade right cascade>
+                <form class="col s12">
+                 
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="email" type="email" class="validate" />
+                      <label for="email">Email</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="password" type="password" class="validate" />
+                      <label for="password">Password</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="Address" type="text" class="validate" />
+                      <label for="Address">Address</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input type="submit" value="Submit" />
+                    </div>
+                  </div>
+                 
+                </form>
+                </Fade>
+              </div>
+            }
+            
+      
         </>
     );
 }
 
 export default Cart;
+
--- src/components/product.js
+++ src/components/product.js
@@ -1,27 +1,42 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import formatCurrency from '../helper/currency';
+import Fade from 'react-reveal/Fade';
 
 function Product(props) {
+
+    
+    const productData = props.data;
+    let data = [];
 
     useEffect(() =>{
         console.log(props.data);
     }) 
 
-    const productData = props.data;
-    let data = [];
+   
+  
     return (
         <div class="row">
             { productData &&
                 productData.map((item, index) => {
                     return <div class="col s12 m4">
+                        <Fade top cascade>
                         <div key={index}>
                             <div class="card" style={{ maxWidth: 300 }}>
                                 <div class="card-image waves-effect waves-block waves-light">
                                     <img class="activator" style={{ maxWidth: 350 }} src={item.image} />
+                                    
                                 </div>
                                 <div class="card-content">
                                     <h6 class="card-title activator grey-text text-darken-4">  {item.title}<i class="material-icons right">more_vert</i></h6>
+                                    <div class="row">
+                                    <div class="col s6">
                                     <p><a href="#">{formatCurrency(item.price)}</a></p>
+                                    </div>
+                                    <div class="col s6">
+                                    <a class="btn-floating btn-large waves-effect waves-light green"><i onClick={() => props.getAddtoCardId(item._id)} class="material-icons">add</i></a>
+                                    </div>
+                                    </div>
+        
                                 </div>
                                 <div class="card-reveal">
                                     <h6 class="card-title grey-text text-darken-4">{item.title}<i class="material-icons right">close</i></h6>
@@ -29,11 +44,16 @@ function Product(props) {
                                 </div>
                             </div>
                         </div>
-
+                        </Fade>
                     </div>
+                  
                 })
             }
+    
         </div>
+
+
+
     );
 }
 
